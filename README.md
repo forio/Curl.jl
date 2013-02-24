9dc5bb38147486260724daed3d39ff601c181a07
Paul Howe <phowe@forio.com>
2013-02-23 19:31:10 -0800

    Updated api to return a response object, better handling of response headers

--- README.md
+++ README.md
@@ -10,30 +10,49 @@ A little Julia client HTTP library. Curl.jl supports basic HTTP method usage
 
   julia> using JSON
 
-  julia> Curl.get("http://jsonip.com")
+  julia> r = Curl.get("http://jsonip.com")
+  Response("{\"ip\":\"24.4.140.175\",\"about\":\"/about\"}",["HTTP/1.1 200 OK", "Server: nginx/1.2.6", "Date: Sun, 24 Feb 2013 02:36:34 GMT"  …  "Access-Control-Allow-Origin: *", "Access-Control-Allow-Methods: GET"])
+
+  julia> r.text
   "{\"ip\":\"24.4.140.175\",\"about\":\"/about\"}"
 
-  julia> JSON.parse(Curl.get("http://jsonip.com"))["ip"]
+  julia> r.headers[1]
+  9-element String Array:
+   "HTTP/1.1 200 OK"                    
+   "Server: nginx/1.2.6"                
+   "Date: Sun, 24 Feb 2013 03:24:08 GMT"
+   "Content-Type: application/json"     
+   "Transfer-Encoding: chunked"         
+   "Connection: keep-alive"             
+   "Vary: Accept-Encoding"              
+   "Access-Control-Allow-Origin: *"     
+   "Access-Control-Allow-Methods: GET"  
+
+  julia> JSON.parse(Curl.get("http://jsonip.com").text)["ip"]
   "24.4.140.175"
 
-  julia> Curl.post("http://requestb.in/181n1gk1", { :arg1 => "var1" })
+  julia> Curl.post("http://requestb.in/181n1gk1", { :arg1 => "var1" }).text
   "ok\n"
 
-  julia> Curl.delete("http://requestb.in/181n1gk1")
+  julia> Curl.delete("http://requestb.in/181n1gk1").text
   "ok\n"
 
   julia> Curl.head("http://requestb.in/181n1gk1")
-  ""
+  Response([["HTTP/1.1 200 OK", "Content-length: 3", "Content-Type: text/html; charset=utf-8", "Date: Sun, 24 Feb 2013 03:27:20 GMT", "Connection: keep-alive"]],"")
 
-  julia> Curl.get("http://nytimes.com")[1:92]
+  julia> Curl.get("http://nytimes.com").text[1:92]
   "<!DOCTYPE html>\n<!--[if IE]><![endif]--> \n<html lang=\"en\">\n<head>\n<title>The New York Times "
+
+  julia> Curl.get("http://nytimes.com").headers
+  2-element Array{String,1} Array:
+   ["HTTP/1.1 302 Found", "Date: Sun, 24 Feb 2013 03:29:06 GMT", "Server: Apache"  …  "Connection: close", "Content-Type: text/html; charset=iso-8859-1"]
+   ["HTTP/1.1 200 OK", "Date: Sun, 24 Feb 2013 03:29:06 GMT", "Server: Apache"  …  "Connection: close", "Content-Type: text/html; charset=UTF-8"]        
 
   ```
 
 ### TODO
 
  * Support for PUT
- * Setting / getting headers
 
 ### Requirements
 
--- src/Curl.jl
+++ src/Curl.jl
@@ -1,23 +1,29 @@
 # LibCurl Julia bindings
 
 # TODO
-# * Method abstraction for libcurl ccall's
-# * tests
+#
 # * PUTs
-# * response format that gives body and headers
+# * tests
+# * Method abstraction for libcurl ccall's
 
 module Curl
 
-resp_data = ""
 include("constants.jl")
 
 const DEBUG = false
 const version = "0.0.1"
 
-function read_c_str(c_str::Ptr{Uint8})
+# global
+raw_response = { :headers => "", :text => "" }
 
-  j_str = ""
+type Response
+  headers::Array{Array{String,1}}
+  text::String
+end
+
+function read_c_str(c_str::Ptr{Uint8})
   i = 1
+  j_str = ""
   while true
     c = char(parse_int(base(10, unsafe_ref(c_str, i))))
     if c == '\0'
@@ -39,7 +45,22 @@ function dict_to_query_params(curl, d::Dict)
   join([join([string(x), escape(curl, d[x])], "=") for x=keys(d)], "&")
 end
 
+function dict_to_query_params(curl, d::Array{Any,1})
+  ""
+end
+
 function write_function(buf::Ptr{Uint8}, size::Uint32, nmemb::Uint32, userp::Ptr{Void})
+  global resp_text
+  gen_write_cb(:text, buf, size, nmemb, userp)
+  nmemb
+end
+
+function header_function(buf::Ptr{Uint8}, size::Uint32, nmemb::Uint32, userp::Ptr{Void})
+  gen_write_cb(:headers, buf, size, nmemb, userp)
+  nmemb
+end
+
+function gen_write_cb(resp_type::Symbol, buf::Ptr{Uint8}, size::Uint32, nmemb::Uint32, userp::Ptr{Void})
 
   # To test, can call directly as:
   # ccall(c_write_function, (Uint32), (Ptr{Uint32}, Uint32, Uint32, Ptr{Void}), 0x00, 0x00, 0x00, 0x00)
@@ -54,14 +75,15 @@ function write_function(buf::Ptr{Uint8}, size::Uint32, nmemb::Uint32, userp::Ptr
     arr[i] = c
   end
   str_val = join(arr)
-  global resp_data = "$resp_data$str_val"
+  global raw_response
+  raw_response[resp_type] = string(raw_response[resp_type], str_val)
 
   if DEBUG
     println("size: $size")
     println("nmemb $nmemb")
     println("num bytes: $num_bytes")
     println("length of str val: $(length(str_val))")
-    println("length of resp data: $(length(resp_data))")
+    println("length of resp data: $(length(raw_response[resp_type]))")
   end
 
   # just report back that we 
@@ -70,6 +92,7 @@ function write_function(buf::Ptr{Uint8}, size::Uint32, nmemb::Uint32, userp::Ptr
 
 end
 c_write_function = cfunction(write_function, Uint32, (Ptr{Uint8}, Uint32, Uint32, Ptr{Void}))
+c_header_function = cfunction(header_function, Uint32, (Ptr{Uint8}, Uint32, Uint32, Ptr{Void}))
 
 function curl_version()
   curl_ver = bytestring(ccall( (:curl_version, "libcurl"), Ptr{Uint8}, ()))
@@ -77,7 +100,8 @@ function curl_version()
 end
 
 function setup_curl()
-  global resp_data = ""
+  global raw_response = { :headers => "", :text => "" }
+
   curl_ver = curl_version()
   if DEBUG; println("curl version: $curl_ver"); end
   curl = ccall( (:curl_easy_init, "libcurl"), Ptr{Uint8}, ())
@@ -85,37 +109,40 @@ function setup_curl()
 end
 
 function setup_curlopts(curl, url)
+  iostr = IOString()
 
   ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Int), curl, CURLOPT_FOLLOWLOCATION, 1)
-  # ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Ptr{Uint8}), curl, CURLOPT_URL, "http://jsonip.com".data)
-  # ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Ptr{Uint8}), curl, CURLOPT_URL, "http://hckrn.ws".data)
+
+  # instruct libcurl to include the headers in the start of the body output
+  ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Int), curl, CURLOPT_HEADER, 0)
+  ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Int), curl, CURLOPT_HEADERFUNCTION, c_header_function)
+
   ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Ptr{Uint8}), curl, CURLOPT_URL, url.data)
-  iostr = IOString()
   ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Int), curl, CURLOPT_WRITEFUNCTION, c_write_function)
   ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Ptr{Uint8}), curl, CURLOPT_FILE, iostr.data)
+end
 
-  # to file
-  # fh = ccall( (:fopen, "libc"), Ptr{Uint8}, (Ptr{Uint8}, Ptr{Uint8}), "out".data, "wb".data)
-  # ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Int), curl, CURLOPT_WRITEFUNCTION, C_NULL)
-  # ccall( (:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Ptr{Uint8}), curl, CURLOPT_FILE, fh)
-  # ccall( (:fclose, "libc"), Ptr{Uint8}, (Ptr{Uint8},), fh)
-
+function parse_headers(raw_headers)
+  raw_header_groups = filter((x)->length(x) > 0, split(raw_headers, "\r\n\r\n"))
+  [split(group, "\r\n") for group in raw_header_groups]
 end
 
 function do_curl(curl)
+  curl_resp = ccall( (:curl_easy_perform, "libcurl"), Ptr{Uint8}, (Ptr{Uint8},), curl)
+  global raw_response
+  # response values are now ready
+  if DEBUG; println("result: $curl_resp"); end
 
-  res = ccall( (:curl_easy_perform, "libcurl"), Ptr{Uint8}, (Ptr{Uint8},), curl)
-  global resp_data
-  # resp_data is now ready
-  if DEBUG; println("result: $res"); end
+  headers = parse_headers(raw_response[:headers])
+  response = Response(headers, raw_response[:text])
 
-  res
+  err = ccall( (:curl_easy_strerror, "libcurl"), Ptr{Uint8}, (Ptr{Uint8},), curl_resp)
+  if DEBUG; println("error: $(bytestring(err))"); end
 
+  response
 end
 
-function cleanup_curl(curl, res)
-  err = ccall( (:curl_easy_strerror, "libcurl"), Ptr{Uint8}, (Ptr{Uint8},), res)
-  if DEBUG; println("error: $(bytestring(err))"); end
+function cleanup_curl(curl)
   ccall( (:curl_easy_cleanup, "libcurl"), Ptr, (Ptr{Uint8},), curl)
 end
 
@@ -134,43 +161,39 @@ macro run_with_block(expr)
     # $(esc(expr))
     $expr
 
-    res = do_curl(curl)
-    cleanup_curl(curl, res)
+    response = do_curl(curl)
+    cleanup_curl(curl)
+    response
 
   end
 end
 
 function head(url)
-  @run_with_block begin 
+  response = @run_with_block begin 
     ccall((:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Int), curl, CURLOPT_NOBODY, 1)
   end
-  resp_data
+  response
 end
 
 function get(url)
   @run_with_block begin end
-  resp_data
 end
 
 function post(url, params)
-
-  @run_with_block begin
+  response = @run_with_block begin
     escaped_params = dict_to_query_params(curl, params)
     # curl_easy_setopt(curl, CURLOPT_POSTFIELDS, "name=daniel&project=curl");
     ccall((:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Ptr{Uint8}), curl, CURLOPT_POSTFIELDS, escaped_params.data)
   end
-  resp_data
-
+  response
 end
 
 function delete(url)
-
-  @run_with_block begin
+  response = @run_with_block begin
     # curl_easy_setopt(handle, CURLOPT_CUSTOMREQUEST, "DELETE"); 
     ccall((:curl_easy_setopt, "libcurl"), Ptr{Uint8}, (Ptr{Uint8}, Int, Ptr{Uint8}), curl, CURLOPT_CUSTOMREQUEST, "DELETE".data)
   end
-  resp_data
-
+  response
 end
 
 end
